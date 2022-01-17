81eb8c44c9eb7a082b6e0a6c97cc40714e414c96
Kyaw Thet Paing <96642770+kyawthetpaingme@users.noreply.github.com>
2022-01-17 21:11:53 +0700

    This is me

---
+++ js/music-list.js
@@ -0,0 +1,74 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Sunflower",
+    artist: "Post Malone, Swae Lee",
+    img: "Sunflower",
+    src: "Sunflower"
+  },
+  {
+    name: "abcdefu",
+    artist: "Gayle",
+    img: "abcdefu",
+    src: "abcdefu"
+  },
+  {
+    name: "Ghost",
+    artist: "Justin Beiber",
+    img: "ghost",
+    src: "ghost"
+  },
+  {
+    name: "Build a bitch",
+    artist: "Bella Poarch",
+    img: "build_a_bitch",
+    src: "build_a_bitch"
+  },
+  {
+    name: "good 4 u",
+    artist: "Olivia Rodrigo",
+    img: "good_4_u",
+    src: "good_4_u"
+  },
+  {
+    name: "Driver License",
+    artist: "Olivia Rodrigo",
+    img: "good_4_u",
+    src: "driver_license"
+  },
+  {
+    name: "On The Ground",
+    artist: "Rose",
+    img: "on_the_ground",
+    src: "on_the_ground"
+  },
+  {
+    name: "Peaches",
+    artist: "Justin Beiber",
+    img: "Peaches",
+    src: "Peaches"
+  },
+  {
+    name: "Stay",
+    artist: "Justin Bieber, The Kid LAROI",
+    img: "Stay",
+    src: "Stay"
+  },
+  {
+    name: "Tie Me Down",
+    artist: "Gryffin",
+    img: "tie_me_down",
+    src: "tie_me_down"
+  },
+
+];
